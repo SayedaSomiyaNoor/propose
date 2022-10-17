9ab564a76e3d99e23928d4fb1b5446c9107a54bd
SayedaNoor <96574987+SayedaNoor@users.noreply.github.com>
2022-10-17 13:05:35 +0600

    done

---
+++ propose.js
@@ -0,0 +1,7 @@
+function clicked(id){
+    const button = document.getElementById(id).addEventListener('click',function(){
+        window.location.href='propose2.html';
+})
+}
+clicked('btn-1');
+clicked('btn-2');
